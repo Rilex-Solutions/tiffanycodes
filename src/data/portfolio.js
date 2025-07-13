2af649f1fc7b2436399f6ef512abfe9d1831d684
tHALL3000 <tiffanyhall.co@gmail.com>
2025-07-12 19:38:24 -0600

    begin building out better structure. starting with utilities and constants.

---
+++ src/data/portfolio.js
@@ -0,0 +1,15 @@
+export const personalInfo = {
+	name: "Tiffany Hall",
+	location: "Available for remote work",
+	email: "tiffanycodes.co@gmail.com",
+	github: "https://github.com/tHALL3000",
+	linkedin: "https://linkedin.com/in/tiffany-hall-colorado",
+};
+
+export const services = [
+
+];
+
+export const projects = [
+
+];
